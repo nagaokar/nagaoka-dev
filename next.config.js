29ba66e980383367ed6f95619dbe73f2cae785ab
nagaokar <63868849+nagaokar@users.noreply.github.com>
2023-05-01 15:38:08 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '',
-      },
     ],
   },
 }
