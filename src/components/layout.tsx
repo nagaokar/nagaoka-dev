c19ff5c5493d3f5ae6ee33f1c678fa9051d70059
nagaokar <63868849+nagaokar@users.noreply.github.com>
2023-05-11 20:33:07 +0100

    home done

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,7 @@ export default function Layout({ children }: any) {
 
   return (
     <main className={`flex flex-col h-screen max-w-screen overflow-x-hidden 
-    bg-white font-regular text-black
+    bg-white font-regular lowercase text-black
       xs:px-4 sm:px-4 md:px-8 lg:px-16
     `}>
       {/* <Nav /> */}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,86 +9,78 @@ import Footer from '@/components/Footer';
 import About from './about';
 import Contact from './contact';
 
+import ghost from '../icons/undraw/undraw_ghost.svg'
+
 export default function Home()
 {
   return (
     <Layout>
         <main>
-          <section id='intro' className='flex flex-row m-auto align-middle justify-center h-screen cursor-default'>
-            <div className='flex flex-col justify-center text-start'>
-              <div className='flex flex-col justify-center text-center p-6 rounded-2xl'>
-                <p className='text-5xl font-normal'>
-                  nagaoka.dev
-                </p>
-                <div className='flex flex-col mt-4 font-light justify-center'>
-                  <p className='mx-4'>Reiji Nagaoka</p>
-                  <p className='mx-4 mt-1'> 長岡礼二</p>
-                </div>
-              </div>
-              {/* <div className='flex flex-wrap w-96 justify-center text-center px-3 bg-white text-gray-600 font-light'>
-              <p className='my-2 mr-3'>Academic</p>
-                <p className='my-2 mr-3'>Computational Designer</p>
-                <p className='my-2 mr-3'>Web Developer</p>
-                <p className='my-2 mr-3'>Writer</p>
-                <p className='my-2 mr-3'>Game-maker</p>
-                <p className='my-2 mr-3'>Musician</p>
-              </div> */}
-{/* 
-              <div className='flex flex-row justify-center font-light text-start bg-white py-6 rounded-b-2xl
-                xs:text-xl sm:text-xl md:text-xl lg:text-2xl xl:text-2xl
-                '>
-
-              </div> */}
-{/*
-              <div className='flex flex-wrap font-light px-0
-                sm:text-xs md:text-base lg:text-lg xl:text-lg
-                xs:justify-center sm:justify-center
-                '>
-                <p className='mt-2 mr-3'>Academic</p>
-                <p className='mt-2 mr-3'>Computational Designer</p>
-                <p className='mt-2 mr-3'>Web Developer</p>
-                <p className='mt-2 mr-3'>Writer</p>
-                <p className='mt-2 mr-3'>Game-maker</p>
-                <p className='mt-2 mr-3'>Musician</p>
-              </div> */}
+          <section id='intro' className='flex h-screen align-middle justify-center'>
+          <div className='flex flex-row justify-center'>
+            
+            {/* INTRO IMAGE*/}
+            <div className='flex flex-row justify-center align-middle'>
+              <Image
+                className='mx-7 translate-y-8
+                xs:w-32
+                sm:w-48
+                md:w-48
+                lg:w-48
+                xl:w-48'
+                src={ghost}
+                width={150}
+                height={150}
+                alt='line drawing of a ghost'
+              />
             </div>
-          </section>
+            
+            {/* INTRO TEXT*/}
+            <div className='flex flex-row justify-center align-middle'>
+              <div className='flex flex-col justify-center font-bold'>
+                <p className='
+                origin-top-left -rotate-12 
+                -translate-x-8 translate-y-2 
+                xs:text-7xl 
+                sm:text-8xl 
+                md:text-4xl  
+                lg:text-6xl  
+                xl:text-8xl'> 
+                  hi!
+                </p>
+                <p className='
+                xs:text-xl 
+                sm:text-2xl  
+                md:text-3xl  
+                lg:text-4xl  
+                xl:text-3xl'>
+                  I&apos;m reiji nagaoka.   
+                </p>
+                <p className='
+                font-thin
+                xs:text-md xs:w-48
+                sm:text-lg sm:w-64
+                md:text-lg md:w-64
+                lg:text-xl lg:w-72
+                xl:text-2xl xl:w-72
+                '>
+                  scholar, game maker, web developer, writer.
+                </p>
+              </div>
+            </div>
 
-          <section>
-            <About />
-          </section>
 
-          <section id='work'>
-          <Employment />
+          </div>
           </section>
-
-          <section id='publications'>
-          <Publications/>
-          </section>
-
-          <section id='talks'>
-          <Talks/>
-          </section>
-
-          <section id='games'>
-          <Games/>
-          </section>
-
-          <section id='contact'>
-            <Contact />
-          </section>
-
-        <section id='footer' className='my-2'>
-          <Footer />
-        </section>
         </main>
     </Layout>
   )
 }
 
+{/* <section id='intro' className='flex flex-row m-auto align-middle justify-center h-screen cursor-default'>
+<div className='flex flex-col justify-center text-start'>
+<div className='flex flex-col justify-start p-6 rounded-2xl'>
 
-// <Link href='#academic'><p className='mx-2'>Academic</p></Link>
-// <Link href='/home'><p className='mx-2'>Web Developer</p></Link>
-// <Link href='/home'><p className='mx-2'>Digital Engineer</p></Link>
-// <Link href='/home'><p className='mx-2'>Writer</p></Link>
-// <Link href='/home'><p className='mx-2'>Game-maker</p></Link>
+</div>
+</div>
+</section>   */}
