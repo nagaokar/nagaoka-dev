e96abafa9adf99aae9f265bd028e847d3d9b39a4
nagaokar <63868849+nagaokar@users.noreply.github.com>
2023-05-12 23:30:58 +0100

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,22 @@
 import React from "react"
 import Image from "next/image"
 import ghost from '../icons/undraw/undraw_ghost.svg'
+import heart from '../icons/undraw/undraw_heart-fun.svg'
 
 export default function Footer()
 {
     return(
-        <main className='flex flex-col'>
-            <div className="flex flex-row justify-center">
-                <Image src={ghost} width={20} height={20} alt="black and white line drawing of a ghost"/>
-                <div className="flex flex-row text-sm ml-2">
-                    <p>nagaoka.dev <span className="text-xs">2023</span>.
-                    
-                    </p>
+        <footer className="flex text-center justify-center w-screen">
+            <div className="flex flex-col">
+                <div className="flex flex-row text-center justify-center">
+                    <Image className="mr-2"
+                    src={ghost} width={20} height={20} alt="black and white line drawing of a ghost"/>
+                    <p className="mr-1">nagaoka.dev</p>
+                </div>
+                <div className="flex flex-row text-xs text-center justify-center px-2 mb-2">
+                    <p>website made with react, nextjs, tailwindcss, and love.</p>
                 </div>
             </div>
-        </main>
+        </footer>
     )
 }
