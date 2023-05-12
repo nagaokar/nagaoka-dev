21d709a9b48cecfac34f3f14e6953c8a2781d3a6
nagaokar <63868849+nagaokar@users.noreply.github.com>
2023-05-13 00:28:36 +0100

    added jobOps bulletin on aboutpage

--- src/components/content/profile.tsx
+++ src/components/content/profile.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import TreeIcon from '../../icons/undraw/undraw_tree.svg';
 import CodeIcon from '../../icons/undraw/undraw_code.svg';
 import CurvedUnderline from '../../icons/undraw/undraw_curved-underline.svg'
+import ExclaimMark from '../../icons/undraw/undraw_exclamation-point(1).svg'
+import StarSpacer from "../StarSpacer";
 
 export default function Profile() {
     return(
@@ -28,6 +30,32 @@ export default function Profile() {
         xs:w-full sm:full md:translate-x-1/4 lg:translate-x-1/4 xl:translate-x-1/4 2xl:translate-x-1/4
         xs:text-xl sm:text-xl md:text-xl lg:text-lg xl:text-xl 2xl:text-xl 
         '>
+
+          {/* START: CURRENT JOB ACCEPTANCE */}
+          <div className="flex flex-col m-3">
+            {/* <div className="flex justify-center mb-2">
+              <Image src={ExclaimMark} width={25} height={25} alt="line drawing of an exclaimation mark"/>
+            </div> */}
+            {/* START: CURRENT JOB ACCEPTANCE TITLE */}
+            <div className="flex flex-col justify-center mt-3 text-center">
+                <p className="font-bold text-xl underline decoration-2">summer 2023</p>
+                <p>if you&apos;d like to hire me, i am currently able to take on:</p>
+            </div>
+            {/* END: CURRENT JOB ACCEPTANCE TITLE */}
+            {/* START: LIST OF PROJECTS */}
+              <div className="flex justify-center font-normal text-start">
+                <ul className="list-disc my-2">
+                  <li>writing videogame-related articles</li>
+                  <li>small web design projects (design/dev)</li>
+                </ul>
+              </div>
+              {/* END: LIST OF PROJECTS */}
+              <div className="mt-6">
+                <StarSpacer />
+              </div>
+            </div>
+          {/* END: CURRENT JOB ACCEPTANCE */}
+
           {/* START: TAGLINE */}
           <div className="flex flex-wrap font-bold justify-center align-middle text-center px-2 w-content 
           mt-7 mb-5 xs:text-start sm:text-start md:text-center lg:text-center xl:text-center 2xl:text-center
@@ -38,7 +66,8 @@ export default function Profile() {
             </p>
           </div>
           {/* END: TAGLINE CONTAINER*/}
-          
+
+
           {/* START: PROFILES CONTAINER */}
           <div className='flex font-normal justify-center align-items-center text-start mt-5
             xs:flex-wrap sm:flex-wrap md:flex-nowrap lg:flex-nowrap xl:flex-nowrap 2xl:flex-nowrap
@@ -87,7 +116,7 @@ export default function Profile() {
                 game development projects use unity in c# and multiplayer games use unity netcode for gameobjects.
               </p>
               <p>
-                my current full stack web development projects use react and next.js, previously they used create-react-app with express.js and react router.
+                currently, i use figma for ui/ux protyping then react, next.js, tailwindcss for frontend development.
               </p>
             </div>
             {/* END: DEV PROFILE CONTAINER */}
