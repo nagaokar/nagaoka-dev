e275360ef9f9c1ba97cf54b0d15384acf15eedd4
nagaokar <63868849+nagaokar@users.noreply.github.com>
2023-05-13 13:27:43 +0100

    mobile-format fix

--- src/components/cards/AcademicCard.tsx
+++ src/components/cards/AcademicCard.tsx
@@ -1,36 +1,32 @@
 import React from "react";
 import { useState } from "react";
 import Image from "next/image";
-import { academics, Academic, AcademicPostProps } from "../../lib/ContentDB";
+import { AcademicPostProps } from "../../lib/ContentDB";
 
 import CircledArrow from "../../icons/undraw/undraw_circled-arrow.svg";
 
 export function AcademicCard(props: AcademicPostProps) {
-  const [open, setOpen] = useState(false);
   return (
     <div
       className="my-1 flex flex-row
-					xs:text-lg sm:text-lg md:text-lg lg:text-lg xl:text-lg 2xl:text-lg"
-    >
+					xs:text-md sm:text-md md:text-lg lg:text-lg xl:text-lg 2xl:text-lg">
       {/* START: CARD DATE */}
-      <div className="mr-2 flex w-12 flex-col font-bold text-red-500">
+      <div className="flex flex-col w-10 text-red-500 px-2 mr-3">
         <p>{props.date}</p>
       </div>
       {/* END: CARD DATE */}
       {/* START: CARD TITLE CONTAINER*/}
       <div
-        className="mb-1 flex w-72 flex-row border-b-2 border-black px-2 pb-2
-				xs:pr-12 sm:pr-10 md:pr-8 lg:pr-6 xl:pr-12"
-      >
+        className="flex w-72">
         {/* START: CARD TITLE SUB-CONTAINER */}
-        <div className="flex flex-col">
+        <div className="flex flex-col mx-4 border-b-2 border-black">
           {/* CARD TITLE */}
           <p className="font-bold">{props.title}</p>
           {/* CARD ORGANISATION */}
           <p>{props.organisation}</p>
           {/* START: MORE INFO LINK */}
           {props.link && (
-            <div className="mt-2 flex flex-row align-middle">
+            <div className="mb-1 flex flex-row align-middle">
               <div className="flex flex-row">
                 <a href={props.link} target="_blank" rel="noopener noreferrer">
                   more info.
--- src/components/content/work.tsx
+++ src/components/content/work.tsx
@@ -4,61 +4,58 @@ import Image from "next/image";
 import Employment from "./employment";
 import Academic from "./academic";
 import Education from "./education";
-import Games from './games';
+import Games from "./games";
 
-import StarIcon from '../../icons/undraw/undraw_star.svg'
+import StarIcon from "../../icons/undraw/undraw_star.svg";
 
 export default function Work() {
-    return(
-        <>
-            {/* START: WORK SECTION */}
-            <section>
-                {/* START: WORK TITLE */}       
-                <div className="flex flex-row w-screen justify-center">
-                    <div className='font-bold text-5xl mt-2 mb-24 text-center -rotate-2'>
-                        <p className="translate-y-24">
-                            cv
-                        </p>
-                        <Image
-                        src={StarIcon} width={125} height={100} alt="a line drawn star"
-                        />
-                    </div>
-                </div>
-
-
-                {/* END: PAGE TITLE */}
-
-                {/* START: WORK CONTENT CONTAINER */}
-                <div className="flex flex-row flex-wrap justify-center">
-                    
-                    {/* START: WORK SUB-CONTAINER 1 */}
-                    <div className="flex flex-col">
-                        {/* ACADEMIC COMPONENT*/}
-                        <div className="flex mx-8">
-                            <Academic />
-                        </div>
-                        {/* GAMES COMPONENT*/}
-                        <div className="flex mx-8">
-                            <Games />
-                        </div>
-                    </div>
-                    {/* END: WORK SUB-CONTAINER 1*/}
-                    {/* START: WORK SUB-CONTAINER 2*/}
-                    <div className="flex flex-col">
-                        {/*EMPLOYMENT COMPONENT*/}
-                        <div className="flex mx-8">
-                            <Employment />
-                        </div>
-                        {/* EDUCATION COMPONENT */}   
-                        <div className="flex mx-8">
-                            <Education />
-                        </div>          
-                    </div>
-                    {/* END: WORK SUB-CONTAINER 2*/}
-                </div>
-                {/* END: WORK CONTENT CONTAINER */}
-            </section>
-            {/* END: WORK SECTION */}
-        </>
-    )
+  return (
+    <>
+      {/* START: WORK SECTION */}
+      <section>
+        {/* START: WORK TITLE */}
+        <div className="flex w-screen flex-row justify-center">
+          <div className="mb-24 mt-2 -rotate-2 text-center text-5xl font-bold">
+            <p className="translate-y-24">cv</p>
+            <Image
+              src={StarIcon}
+              width={125}
+              height={100}
+              alt="a line drawn star"
+            />
+          </div>
+        </div>
+        {/* END: PAGE TITLE */}
+        {/* START: WORK CONTENT CONTAINER */}
+        <div className="flex flex-col flex-wrap justify-center">
+          {/* START: WORK SUB-CONTAINER 1 */}
+          <div className="flex flex-col">
+            {/* ACADEMIC COMPONENT*/}
+            <div className="flex">
+              <Academic />
+            </div>
+            {/* GAMES COMPONENT*/}
+            <div className="flex">
+              <Games />
+            </div>
+          </div>
+          {/* END: WORK SUB-CONTAINER 1*/}
+          {/* START: WORK SUB-CONTAINER 2*/}
+          <div className="flex flex-col">
+            {/*EMPLOYMENT COMPONENT*/}
+            <div className="flex">
+              <Employment />
+            </div>
+            {/* EDUCATION COMPONENT */}
+            <div className="flex">
+              <Education />
+            </div>
+          </div>
+          {/* END: WORK SUB-CONTAINER 2*/}
+        </div>
+        {/* END: WORK CONTENT CONTAINER */}
+      </section>
+      {/* END: WORK SECTION */}
+    </>
+  );
 }
