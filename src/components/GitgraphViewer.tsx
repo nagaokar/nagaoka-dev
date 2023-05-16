747a109207cb0bf3dbeb4c0dd3eebc2f95395158
nagaokar <63868849+nagaokar@users.noreply.github.com>
2023-05-16 18:53:12 +0100

    exporting single branch; wip export all branches

--- src/components/GitgraphViewer.tsx
+++ src/components/GitgraphViewer.tsx
@@ -4,6 +4,7 @@ export interface Commit {
   sha: string;
   message: string;
   branch: string;
+  author: string;
   tag: string;
   date: string;
 }
@@ -21,10 +22,17 @@ const GitgraphViewer: React.FC = () => {
   }, []);
 
   return (
-    <div>
-      <ul className='list-disc'>
+    <div id="gitrepoViewer" className='w-content mx-4'>
+      <ul className=''>
         {commits.map((commit) => (
-          <li key={commit.sha}>{commit.message}</li>
+          <li key={commit.sha} className='my-2'>
+            <p>{commit.sha}</p>
+            <p className='text-red-500'>{commit.author}</p>
+            <p>{commit.date}</p> 
+            <p>{commit.message}</p>
+            <p>{commit.date}</p>
+            <p id="branchName" className='font-bold'>{commit.branch}</p>
+            </li>
         ))}
       </ul>
     </div>
--- src/pages/api/repogitlog.tsx
+++ src/pages/api/repogitlog.tsx
@@ -1,23 +1,45 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import gitlog from 'gitlog';
+import gitlog, { GitlogOptions } from 'gitlog';
 import path from 'path';
 
 interface Commit {
   sha: string;
   message: string;
+  branch: string;
 }
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse): Promise<void> {
   const repoPath = path.resolve(process.cwd(), '..\\nagaoka-dev'); // Corrected path with escaped backslashes
+  
+  const branches = [
+    "main",
+    "develop/layout", 
+    "develop/work", 
+    "feature/gitgraph", 
+    "feature/spa",
+  ] 
+  
   const options = {
     repo: repoPath,
-    number: 10, // Limit the number of commits to fetch
+    number: 10,
+    branch: "main",
+    fields: [
+      "subject", 
+      "authorName", 
+      "authorDate",
+      "abbrevHash",
+      "tag",
+    ] as const, 
   };
 
   const commitHistory = gitlog(options);
-  const formattedCommits: Commit[] = commitHistory.map((commit) => ({
-    sha: commit.hash,
+  const formattedCommits: GitlogOptions[] = commitHistory.map((commit) => ({
+    repo: repoPath,
+    sha: commit.abbrevHash,
     message: commit.subject,
+    author: commit.authorName,
+    date: commit.authorDate,
+    tag: commit.tag,
   }));
 
   res.status(200).json(formattedCommits);
