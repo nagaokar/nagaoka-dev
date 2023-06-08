cf6df8d04b9ddaf2e3347583b383591debb6312d
nagaokar <63868849+nagaokar@users.noreply.github.com>
2023-06-08 14:25:31 +0100

    removed note in about

---
+++ src/components/Note.tsx
@@ -0,0 +1,18 @@
+// <div className="flex justify-center">
+//   <div className="max-w-144 sm:text-md my-6 flex flex-col justify-center p-6 text-center text-2xl font-bold">
+//     <Spacer />
+//     <div className="my-4">
+//       <p className="my-2 bg-white text-xl font-bold underline decoration-2">
+//         note: summer <span className="text-sm">2023</span>
+//       </p>
+//       <p>
+//         if you&apos;d like to hire me, i am currently able to take on:{' '}
+//       </p>
+//       <ul className="my-3 font-normal">
+//         <li>videogame related articles</li>
+//         <li>small web design projects (design/dev)</li>
+//       </ul>
+//     </div>
+//     <Spacer />
+//   </div>
+// </div>
